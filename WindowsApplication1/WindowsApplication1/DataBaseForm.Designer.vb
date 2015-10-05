259344ee319a8de0c92f529edf63eb00bce4ba0d
Robert Peck <rpeck0914@mail.kvcc.edu>
2015-10-04 20:55:38 -0400

    Windows application that pulls and sorts information from a database

---
+++ WindowsApplication1/WindowsApplication1/DataBaseForm.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DataBaseForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BindButton = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.FilterButton = New System.Windows.Forms.Button()
+        Me.FilterTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProductNameTextBox = New System.Windows.Forms.TextBox()
+        Me.UnitPriceTextBox = New System.Windows.Forms.TextBox()
+        Me.InsertButton = New System.Windows.Forms.Button()
+        Me.DeleteButton = New System.Windows.Forms.Button()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.ProductIDLabel = New System.Windows.Forms.Label()
+        Me.IDTextBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearDataButton = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BindButton
+        '
+        Me.BindButton.Location = New System.Drawing.Point(12, 110)
+        Me.BindButton.Name = "BindButton"
+        Me.BindButton.Size = New System.Drawing.Size(99, 23)
+        Me.BindButton.TabIndex = 11
+        Me.BindButton.Text = "&Bind To List"
+        Me.BindButton.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 147)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(130, 394)
+        Me.ListBox1.TabIndex = 14
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(148, 139)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(381, 402)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'FilterButton
+        '
+        Me.FilterButton.Location = New System.Drawing.Point(254, 110)
+        Me.FilterButton.Name = "FilterButton"
+        Me.FilterButton.Size = New System.Drawing.Size(75, 23)
+        Me.FilterButton.TabIndex = 13
+        Me.FilterButton.Text = "&Filter"
+        Me.FilterButton.UseVisualStyleBackColor = True
+        '
+        'FilterTextBox
+        '
+        Me.FilterTextBox.Location = New System.Drawing.Point(148, 112)
+        Me.FilterTextBox.Name = "FilterTextBox"
+        Me.FilterTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.FilterTextBox.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Product Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Unit Price:"
+        '
+        'ProductNameTextBox
+        '
+        Me.ProductNameTextBox.Location = New System.Drawing.Point(96, 16)
+        Me.ProductNameTextBox.Name = "ProductNameTextBox"
+        Me.ProductNameTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.ProductNameTextBox.TabIndex = 1
+        '
+        'UnitPriceTextBox
+        '
+        Me.UnitPriceTextBox.Location = New System.Drawing.Point(96, 42)
+        Me.UnitPriceTextBox.Name = "UnitPriceTextBox"
+        Me.UnitPriceTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.UnitPriceTextBox.TabIndex = 3
+        '
+        'InsertButton
+        '
+        Me.InsertButton.Location = New System.Drawing.Point(230, 14)
+        Me.InsertButton.Name = "InsertButton"
+        Me.InsertButton.Size = New System.Drawing.Size(99, 23)
+        Me.InsertButton.TabIndex = 4
+        Me.InsertButton.Text = "&Insert"
+        Me.InsertButton.UseVisualStyleBackColor = True
+        '
+        'DeleteButton
+        '
+        Me.DeleteButton.Location = New System.Drawing.Point(230, 72)
+        Me.DeleteButton.Name = "DeleteButton"
+        Me.DeleteButton.Size = New System.Drawing.Size(99, 23)
+        Me.DeleteButton.TabIndex = 6
+        Me.DeleteButton.Text = "&Delete"
+        Me.DeleteButton.UseVisualStyleBackColor = True
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(230, 43)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(99, 23)
+        Me.UpdateButton.TabIndex = 5
+        Me.UpdateButton.Text = "&Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'ProductIDLabel
+        '
+        Me.ProductIDLabel.AutoSize = True
+        Me.ProductIDLabel.Location = New System.Drawing.Point(335, 48)
+        Me.ProductIDLabel.Name = "ProductIDLabel"
+        Me.ProductIDLabel.Size = New System.Drawing.Size(61, 13)
+        Me.ProductIDLabel.TabIndex = 7
+        Me.ProductIDLabel.Text = "Product ID:"
+        '
+        'IDTextBox
+        '
+        Me.IDTextBox.Location = New System.Drawing.Point(402, 45)
+        Me.IDTextBox.Name = "IDTextBox"
+        Me.IDTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.IDTextBox.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(335, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "*ID Required For Update"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(335, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(111, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = " And Deleting Of Data"
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(442, 553)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(87, 40)
+        Me.ExitButton.TabIndex = 16
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearDataButton
+        '
+        Me.ClearDataButton.Location = New System.Drawing.Point(349, 553)
+        Me.ClearDataButton.Name = "ClearDataButton"
+        Me.ClearDataButton.Size = New System.Drawing.Size(87, 40)
+        Me.ClearDataButton.TabIndex = 17
+        Me.ClearDataButton.Text = "&Clear Data"
+        Me.ClearDataButton.UseVisualStyleBackColor = True
+        '
+        'DataBaseForm
+        '
+        Me.AcceptButton = Me.FilterButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(541, 605)
+        Me.Controls.Add(Me.ClearDataButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.IDTextBox)
+        Me.Controls.Add(Me.ProductIDLabel)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Controls.Add(Me.DeleteButton)
+        Me.Controls.Add(Me.InsertButton)
+        Me.Controls.Add(Me.UnitPriceTextBox)
+        Me.Controls.Add(Me.ProductNameTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BindButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.FilterButton)
+        Me.Controls.Add(Me.FilterTextBox)
+        Me.Name = "DataBaseForm"
+        Me.Text = "Data Base Northwind"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BindButton As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents FilterButton As System.Windows.Forms.Button
+    Friend WithEvents FilterTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ProductNameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents UnitPriceTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents InsertButton As System.Windows.Forms.Button
+    Friend WithEvents DeleteButton As System.Windows.Forms.Button
+    Friend WithEvents UpdateButton As System.Windows.Forms.Button
+    Friend WithEvents ProductIDLabel As System.Windows.Forms.Label
+    Friend WithEvents IDTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents ClearDataButton As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/DataBaseForm.vb
@@ -0,0 +1,204 @@
+﻿' Program: Business Rule & Data Classes
+' Author:  Robert Peck
+' Date:    03/03/2015
+
+Option Explicit On
+Option Strict On
+Imports DataBaseTables.Tables
+
+Public Class DataBaseForm
+
+    Dim TheConnectionString As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=J:\CIS212\RobertPeckProject02\WindowsApplication1\Northwind.mdb;"
+    Private aDataRun As New DataBaseTables.Tables.DataBaseTableSelection(TheConnectionString)
+    Private UnitPrice As Decimal
+    Private IDBoolean As Boolean = False
+    Private IDInput As Integer = Nothing
+
+#Region "   Listbox/Grid Filled via filter textbox   "
+
+    Private Sub FilterButton_Click(sender As Object, e As EventArgs) Handles FilterButton.Click 'When Filter Button Is Clicked Program will Pull Information from the database based on the input filter
+
+
+        Try
+            DataGridView1.DataSource = (aDataRun.FillGrid(FilterTextBox.Text)).Tables(0)
+            ListBox1.Items.Clear()
+            With aDataRun.RetrieveProductNamePrice(FilterTextBox.Text)
+                If .HasRows Then
+                    Do While .Read
+                        ListBox1.Items.Add(.GetString(0))
+                    Loop
+                End If
+            End With
+        Catch ex As Exception
+            MessageBox.Show(aDataRun.ErrorMessage)
+        End Try
+        '    If (aDataRun.GetData(FilterTextBox.Text)) Then  ' Sends the filter over to the business class
+        '        DataGridView1.DataSource = aDataRun.DataSet.Tables(0)       ' Sets the datasource to fill the datebase into the datagrid
+        '        Try
+        '            ListBox1.Items.Clear()
+        '            With aDataRun.DataReader    ' Pulls the data reader to read date for writing into the list box
+        '                If .HasRows Then    'If data reader still has rows it will continue
+        '                    Do While .Read  'Loops through and reads the information for writing
+        '                        ListBox1.Items.Add(.GetString(0))   'adds the item to the list box
+        '                    Loop    ' ends loop
+        '                Else
+        '                    MessageBox.Show("No Data")  ' Message for when there is no data to write
+        '                End If
+        '            End With
+        '        Catch ex As Exception
+        '            MessageBox.Show(ex.Message)     ' Error message if there is an error while running the data reader
+        '        End Try
+        '    Else
+        '        MessageBox.Show(aDataRun.ErrorMessage)  ' Error message if there is an error while writing to the data grid
+        '    End If
+    End Sub
+#End Region
+
+#Region " Fill ListBox w/ DataSet "
+    Private Sub ListBox1_DoubleClick(sender As Object, e As System.EventArgs) Handles ListBox1.DoubleClick
+        ' Pulls the Product ID number when double clicking an item in the list box
+        If ListBox1.SelectedValue Is Nothing Then
+            MessageBox.Show("Selected value is NULL")   ' If there is no Product ID the message will display null
+        Else
+            MessageBox.Show(ListBox1.SelectedValue.ToString)    ' Sets the Product ID into a message box
+        End If
+    End Sub
+    ' When Bind Button is clicked the program will pull all the products in the database into the list box
+    Private Sub BindButton_Click(sender As Object, e As EventArgs) Handles BindButton.Click
+        Try
+
+
+            Select Case True
+                Case aDataRun.FillListBox().Tables().Count = 0
+                    MessageBox.Show("Nothing Found. Table count: 0")
+                Case aDataRun.FillListBox().Tables(0).Rows.Count = 0
+                    MessageBox.Show("Nothing Found. Row Count: 0")
+                Case Else
+                    ListBox1.DisplayMember = "ProductName"
+                    ListBox1.ValueMember = "ProductID"
+                    ListBox1.DataSource = aDataRun.FillListBox().Tables(0)
+            End Select
+        Catch ex As Exception
+            MessageBox.Show(aDataRun.ErrorMessage)
+        End Try
+
+
+        'If (aDataRun.BindListBox()) Then    ' runs the method to pull information from the database
+        '    Select Case True
+        '        Case aDataRun.DataSet.Tables().Count = 0
+        '            MessageBox.Show("Nothing Found. Table count: 0") ' if the data set count is 0 the following message will display
+        '        Case aDataRun.DataSet.Tables(0).Rows.Count = 0
+        '            MessageBox.Show("Nothing Found. Row Count: 0") ' if the row count is 0 the following message will display
+        '        Case Else
+        '            ListBox1.DisplayMember = "ProductName"  ' By default if the table and rows are not 0 the database data will be put into the list box
+        '            ListBox1.ValueMember = "ProductID"
+        '            ListBox1.DataSource = aDataRun.DataSet.Tables(0)
+        '    End Select
+        'Else
+        '    MessageBox.Show(aDataRun.ErrorMessage)  ' If an error occurs in filling the list box the following message will be display
+        'End If
+    End Sub
+#End Region
+
+#Region " Insert Data Into Database "
+    ' When the insert button is clicked the program will attempt to add the data into the database
+    Private Sub InsertButton_Click(sender As Object, e As EventArgs) Handles InsertButton.Click
+        If ValidateInput() Then ' Runs a input validation method to make sure the inputs are valid
+            If (aDataRun.CreateData(ProductNameTextBox.Text, UnitPrice)) = 1 Then    ' runs method to insert the data and sends over the product name and price
+                MessageBox.Show("Data Has Been Added Successfully", "Success", MessageBoxButtons.OK)    ' if insert was successfull the following message will display
+            Else
+                MessageBox.Show(aDataRun.ErrorMessage)  ' if insert faile the following message will display
+            End If
+        End If
+    End Sub
+
+#End Region
+
+#Region " Update Data In Database "
+    ' Update button will update an existing record within the data base, the id will be required to update the record
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        IDBoolean = True    ' Boolean to add the additional validation for validating the id
+        If ValidateInput() Then ' runs the validate input method
+            If (aDataRun.UpdateData(ProductNameTextBox.Text, UnitPrice, IDInput)) = 1 Then   ' if validation successful the program will send over the unitprice and id to update the information
+                MessageBox.Show("Data Has Been Updated Successfully", "Success", MessageBoxButtons.OK)  ' If update is successful the following message will display
+            Else
+                MessageBox.Show(aDataRun.ErrorMessage)  ' If update fails the following message will display
+            End If
+        End If
+        IDBoolean = False   ' resets the id boolean for validation back to false
+    End Sub
+
+#End Region
+
+#Region " Delete Data From Database "
+    ' Delete button will delete data from the database, the id will be required to delete a record
+    Private Sub DeleteButton_Click(sender As Object, e As EventArgs) Handles DeleteButton.Click
+        IDBoolean = True    ' Boolean to add the additional validation for validating the id
+        If ValidateInput() Then ' runs the validate input method
+            If (aDataRun.DeleteData(ProductNameTextBox.Text, IDInput)) = 1 Then ' if validation successful the program will send over the Product name and id to delete the information
+                MessageBox.Show("Data Was Delted Successfully", "Success", MessageBoxButtons.OK)    ' If the delete is successful the following message will display
+            Else
+                MessageBox.Show(aDataRun.ErrorMessage)  ' If Delete fails the following message will display
+            End If
+        End If
+        IDBoolean = False   ' resets the id boolean for validation back to false
+    End Sub
+
+#End Region
+
+#Region " Input Validation "
+    ' Method for validating the user input
+    Private Function ValidateInput() As Boolean
+        If ProductNameTextBox.Text.Trim <> String.Empty Then    ' Makes sure the product name text box is filled
+            Try
+                UnitPrice = Decimal.Parse(UnitPriceTextBox.Text)    ' Parses the unit price and stores it in a variable
+                If IDBoolean Then
+                    Try
+                        IDInput = Integer.Parse(IDTextBox.Text) ' parses the ID and stores it in a variable
+                        Return True ' Returns true to show data is validated
+                    Catch ex As Exception
+                        MessageBox.Show("Please Enter A Vaild ID Number For Product", "ID Number Error", MessageBoxButtons.OK)
+                        With IDTextBox      ' Displays message to user that this input needs to be fixed
+                            .SelectAll()
+                            .Focus()
+                        End With
+                        Return False    ' Returns false if the data input is incorrect
+                    End Try
+                End If
+                Return True ' Returns true when ID isn't in use and data is validated
+            Catch ex As Exception
+                MessageBox.Show("Please Enter A Unit Price As A Decimal", "Unit Price Error", MessageBoxButtons.OK)
+                With UnitPriceTextBox   ' Displays message to user that this input needs to be fixed
+                    .SelectAll()
+                    .Focus()
+                End With
+                Return False    ' Returns false if the data input is incorrect
+            End Try
+        Else
+            MessageBox.Show("Please Enter A Product Name", "Product Name Error", MessageBoxButtons.OK)
+            With ProductNameTextBox ' Displays message to user that this input needs to be fixed
+                .SelectAll()
+                .Focus()
+            End With
+            Return False    ' Returns false if the data input is incorrect
+        End If
+    End Function
+
+#End Region
+
+    Private Sub ClearDataButton_Click(sender As Object, e As EventArgs) Handles ClearDataButton.Click   ' Resets the textboxes to empty
+        ProductNameTextBox.Text = String.Empty
+        UnitPriceTextBox.Text = String.Empty
+        IDTextBox.Text = String.Empty
+        FilterTextBox.Text = String.Empty
+        ProductNameTextBox.Focus()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Close() ' Closes the program
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
